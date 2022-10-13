2e6bac7456b23060421564c99e258ed8a4ccb5d7
Ansh <khandelwalansh8@gmail.com>
2022-10-13 13:40:20 -0700

    Made a start splash screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import EventType, font
+from tkinter import CENTER, EventType, font
 import pygame
 from sys import exit
 
@@ -32,9 +32,46 @@ ground2 = pygame.transform.scale(ground, (800, 100))
 ground2_xpos = 800
 
 #Load score
+file = open('highscore.txt', 'r')
+content = file.readlines()
+if(len(content) == 0):
+    highscore = 0
+else:
+    highscore = int(content[0])
+file.close()
+
 score = 0
 score_font = pygame.font.Font('Pixel.ttf', 50)
 
+#Intro screen fonts
+game_font = pygame.font.Font('Pixel.ttf', 100)
+start_font = pygame.font.Font('Pixel.ttf', 50)
+game_display = game_font.render('Platform', False, 'Black')
+game_display_rect = game_display.get_rect(center = (400, 200))
+start_display = start_font.render('Start', False, 'Black')
+start_rect = start_display.get_rect(center = (400, 300))
+
+#Intro Splash Screen
+exit = False
+while not exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.MOUSEMOTION:
+            if start_rect.collidepoint(event.pos):
+                start_display = start_font.render('Start', False, 'White')
+                start_rect = start_display.get_rect(center = (400, 300))
+            else:
+                start_display = start_font.render('Start', False, 'Black')
+                start_rect = start_display.get_rect(center = (400, 300))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if start_rect.collidepoint(event.pos):
+                exit = True
+
+    screen.blit(bg, (0,0))
+    screen.blit(game_display, game_display_rect)
+    screen.blit(start_display, start_rect)
+    pygame.display.update()
 
 exit = False
 while not exit:
@@ -85,3 +122,8 @@ while not exit:
 
     pygame.display.update() #update entire screen
     clock.tick(60)
+
+if(score > highscore):
+    file = open('highscore.txt', 'w')
+    file.write(str(score))
+    file.close()
